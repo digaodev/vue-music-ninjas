b7e8df7468fdfb9bda54d5a82bd0292d16621d3a
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2021-04-02 15:16:09 -0300

    feat: add ListView component to list all the user's playlists

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,29 @@
 <template>
-  <h1>Home</h1>
+  <div class="home">
+    <div class="error" v-if="error">Could not fetch the data</div>
+    <div v-if="documents">
+      <ListView :playlists="documents" />
+    </div>
+  </div>
 </template>
 
 <script>
+import getCollection from '../composables/getCollection'
+import ListView from '../components/ListView'
+
 export default {
   name: "Home",
-  components: {},
+  components: {ListView},
+  setup() {
+    const {error, documents} = getCollection('playlists')
+
+    return { error, documents }
+  }
 };
 </script>
+
+<style>
+  ul {
+    list-style-type: none;
+  }
+</style>
