91b78c8cbf07e3fb91c329ec87bddd457e852444
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2021-04-02 17:11:22 -0300

    feat: add useDocument composable

---
+++ src/composables/useDocument.js
@@ -0,0 +1,46 @@
+import { ref } from 'vue'
+
+import { db } from '../services/firebase'
+
+const useDocument = (collection, id) => {
+  const error = ref(null)
+  const isPending = ref(false)
+  let docRef = db.collection(collection).doc(id)
+
+  const deleteDoc = async () => {
+    isPending.value = true
+    error.value = null
+
+    try {
+      const res = await docRef.delete()
+      isPending.value = false
+      return res
+    }
+    catch (err) {
+      console.log(err.message)
+      isPending.value = false
+      error.value = 'Oops! Could not delete the document'
+    }
+  }
+
+  const updateDoc = async (updates) => {
+    isPending.value = true
+    error.value = null
+
+    try {
+      const res = await docRef.update(updates)
+      isPending.value = false
+      return res
+    }
+    catch (err) {
+      console.log(err.message)
+      isPending.value = false
+      error.value = 'Oops! Could not update the document'
+    }
+  }
+
+  return { error, isPending, deleteDoc, updateDoc }
+
+}
+
+export default useDocument
